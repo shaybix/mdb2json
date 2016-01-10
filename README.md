12c45ec021dea5db49b91fbe4cb92158aa3281e9
shaybix <shaybix@tuta.io>
2016-01-10 04:31:18 +0000

    errors still need resolving

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
 # mdb2json
 
-
-
 ### checklist
 
 - [x]   Dump schema into Sql Database
 - [ ]   Dump data into Sql Database
+- [ ]   refactor the code, so sql specific functions are in their own go file
 - [ ]   Work out the schema / structure in json
 - [ ]   Extract Database tables and store as json objects in json file
 
--- main.go
+++ main.go
@@ -30,22 +30,40 @@ func main() {
 
 	files := crawlDir("mdb")
 
-	db, err := initSqlDb(*outputFile)
-	if err != nil {
-
-		log.Fatal("Could not initialise database: ", err)
-	}
-
 	for _, file := range files {
 
 		if file == "" {
 			continue
 		}
 
-		err := schema(file, db)
+		newFile := strings.Split(file, ".")[0]
+
+		f, err := os.Create(newFile + ".sqlite")
+
+		defer f.Close()
 
 		if err != nil {
-			log.Printf("Not able to set schema: %v", err)
+			log.Fatalf("Could not create file: %v", err)
+		}
+
+		db, err := initSqlDb(newFile)
+		if err != nil {
+			log.Fatalf("Could not initialise Database: %v", err)
+		}
+
+		defer db.Close()
+		err = schema(file, db)
+
+		if err != nil {
+			log.Printf("Not able to set the schema: %v", err)
+		}
+
+		err = dumpToSql(file, db)
+
+		if err != nil {
+
+			log.Printf("Unable to insert data into database: %v", err)
+
 		}
 	}
 
@@ -75,7 +93,7 @@ func crawlDir(dirname string) []string {
 // schema exports the mdb-schema in json format
 func schema(filename string, db *sql.DB) error {
 
-	out, err := exec.Command("mdb-schema", *dir+"/"+filename, "mysql").Output()
+	out, err := exec.Command("mdb-schema", *dir+"/"+filename, "sqlite").Output()
 	if err != nil {
 
 		log.Print("Could not execute the command: ", err)
@@ -97,8 +115,6 @@ func schema(filename string, db *sql.DB) error {
 		}
 
 	}
-
-	defer db.Close()
 
 	return nil
 
@@ -126,11 +142,9 @@ func initSqlDb(dbFile string) (*sql.DB, error) {
 }
 
 //dump dumps data into a Sql database and  returning an error
-func dumpToSql(filename string) error {
+func dumpToSql(filename string, db *sql.DB) error {
 
-	var err error
-
-	err = prepareEnv()
+	err := prepareEnv()
 	if err != nil {
 
 		log.Fatal(err)
@@ -138,6 +152,39 @@ func dumpToSql(filename string) error {
 	}
 
 	// TODO: Dump the data to the Sqlite DB file
+
+	out, err := exec.Command("mdb-tables", "-1", *dir+"/"+filename).Output()
+	if err != nil {
+		log.Fatalf("Can not execute command: %v", err)
+		return err
+	}
+
+	tables := strings.Split(string(out), "\n")
+	log.Printf("Starting Data Insertion in: %s", filename)
+
+	for _, table := range tables {
+
+		out, err := exec.Command("mdb-export", "-I", "sqlite", *dir+"/"+filename, table).Output()
+		if err != nil {
+
+			log.Fatalf("unable to export mdb as Sql queries: %v", err)
+			return err
+
+		}
+
+		queries := strings.Split(string(out), "\n")
+
+		for _, query := range queries {
+			_, err := db.Exec(query)
+			if err != nil {
+				// Currently throwing unrecognized token error
+				//
+
+				log.Fatalf("Unable to execute query: %v", err)
+				return err
+			}
+		}
+	}
 
 	return nil
 
